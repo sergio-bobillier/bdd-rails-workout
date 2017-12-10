a6d42155e2137ff3f4fa8a103c63b762d2d13399
Sergio Bobillier C <sergio.bobillier@gmail.com>
2017-12-10 09:55:24 -0500

    Implements the user sign-in feature.

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'User can sign-up on the application' do
+RSpec.feature 'Users can sign-up on the application' do
   scenario 'A user signs-up with valid credentials' do
     visit '/'
 
@@ -14,5 +14,26 @@ RSpec.feature 'User can sign-up on the application' do
 
     expect(page.current_path).to eq(root_path)
     expect(page).to have_content 'You have signed up successfully.'
+  end
+end
+
+RSpec.feature 'Users can sign-in into the application' do
+  before do
+    @john = User.create!(email: 'john@example.com', password: 'password')
+  end
+
+  scenario 'A user signs-in with valid credentials' do
+    visit '/'
+
+    click_link 'Sign-in'
+    expect(page.current_path).to eq(new_user_session_path)
+
+    fill_in 'Email', with: @john.email
+    fill_in 'Password', with: @john.password
+    click_button 'Log in'
+
+    expect(page.current_path).to eq(root_path)
+    expect(page).to have_content 'Signed in successfully.'
+    expect(page).to have_content "Signed in as #{@john.email}"
   end
 end
