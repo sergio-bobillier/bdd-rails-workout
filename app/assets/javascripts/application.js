685dc7aea6a66e029e9f9d4686ea76d2fb8a396f
Sergio Bobillier C <sergio.bobillier@gmail.com>
2017-12-10 17:13:13 -0500

    Adds jQuery UI datepicker to the exercises creation form.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery-ui/widgets/datepicker
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
