7571880eb4d5e5d06f8ae73e923812e04b5ed750
Sergio Bobillier C <sergio.bobillier@gmail.com>
2017-12-17 10:19:11 -0500

    Implements the list of members on the home page.

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -9,3 +9,22 @@ RSpec.feature 'Home Page' do
     expect(page).to have_content('Workout Lounge')
   end
 end
+
+RSpec.feature 'Members list on the home page' do
+  before do
+    @members = [
+      User.create!(first_name: 'John', last_name: 'Doe', email: 'john.doe@example.com', password: 'password'),
+      User.create!(first_name: 'Sarah', last_name: 'Joseph', email: 'sarah.joseph@example.com', password: 'password')
+    ]
+  end
+
+  scenario "A user visits the application's home page" do
+    visit '/'
+
+    expect(page).to have_content('List of Members')
+
+    @members.each do |member|
+      expect(page).to have_content(member.full_name)
+    end
+  end
+end
