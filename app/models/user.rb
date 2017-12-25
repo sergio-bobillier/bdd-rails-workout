95ec52d574e1f30e08b2e0fb75f25c005a0bc592
Sergio Bobillier C <sergio.bobillier@gmail.com>
2017-12-25 10:13:47 -0500

    Adds validations for Users' first and last names.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   def full_name
     "#{first_name} #{last_name}"
   end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -22,6 +22,24 @@ RSpec.feature 'Users can sign-up on the application' do
     expect(page).to have_content 'You have signed up successfully.'
     expect(page).to have_content 'John Doe'
   end
+
+  scenario 'A user signs-up with invalid credentials' do
+    visit '/'
+
+    click_link 'Sign-up'
+    expect(page.current_path).to eq(new_user_registration_path)
+
+    fill_in 'First name', with: ''
+    fill_in 'Last name', with: ''
+    fill_in 'Email', with: 'john@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
+
+    expect(page.current_path).to eq(users_path)
+    expect(page).to have_content "First name can't be blank"
+    expect(page).to have_content "Last name can't be blank"
+  end
 end
 
 RSpec.shared_context 'A user already exists' do
